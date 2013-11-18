4c6e05f63514d344bcd8a1c91eed144a879f9285
Student <student@AC02KP02TF67D.local>
2013-11-18 18:04:24 -0500

    education_dotcom_api.py

---
+++ Backend/education_dotcom_api.py
@@ -0,0 +1,9 @@
+#goal: to create an ability to export data from the education api 
+#and host it on our website. 
+
+import json #download json and then import it in order to access functionality.
+
+def run() { 
+    key = "2db9eedc16ba9389991b223c4fb97df8"
+    url = "http://api.education.com/service/service.php?f=schoolSearch&key=<YourAPIKey>&sn=sf&v=4&[parameters]"
+}
