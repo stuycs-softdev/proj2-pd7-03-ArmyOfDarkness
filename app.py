d54e43fb583eaa942d459687f3525332dc089483
takchiwan <ray325789@yahoo.com>
2013-11-18 20:19:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, session, redirect, request, url_for, render_template
+import db
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
@@ -7,6 +7,11 @@ app.secret_key = "secretkey"
 @app.route("/home")
 def home():
     return "<h1>Temp home page for group 3</h1>"
+    
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.debug = True
