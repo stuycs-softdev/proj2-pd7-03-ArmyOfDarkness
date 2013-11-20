9a30902acebc6434f0afefe2cd86ef8f0b513247
takchiwan <ray325789@yahoo.com>
2013-11-19 23:01:03 -0500

    added user functionality

--- app.py
+++ app.py
@@ -6,8 +6,60 @@ app.secret_key = "secretkey"
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html")
-    
+    if 'user' in session:
+        return render_template("index.html", user = session['user']);
+    else:
+        return redirect(url_for('login'))
+        
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    elif request.method == "GET":
+        return render_template("register.html", message = "")
+    else:
+        button = request.form['button'].encode("utf8")
+        if button == "Register":
+            if db.register(request.form['user'], request.form['pass']):
+                session['user'] = request.form['user']
+                return redirect(url_for('home'))
+            else:
+                return render_template("register.html", message = "User already exists. Please login.")
+        else:
+            return render_template("register.html", message = "")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    elif request.method == "GET":
+        return render_template("login.html", message = "")
+    else:
+        user = request.form['user']
+        pw = request.form['pass']
+        if user == "" or pw == "":
+            return render_template("login.html", message = "Please enter your username and password.")
+        elif db.login(user, pw):
+            session['user'] = user
+            return redirect(url_for('home'))
+        else:
+            return render_template("login.html", message = "Invalid username and password combination. Usernames and passwords are case sensitive. Please try again.")
+
+@app.route("/account", methods = ['GET', 'POST'])
+def account():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    elif request.method == "GET":
+        return render_template("account.html", message = "")
+    else:
+        user = session['user']
+        old = request.form['old']
+        new = request.form['new']
+        if db.changePass(user, old, new):
+            return render_template("account.html", message = "Password changed successfully.")
+        else:
+            return render_template("account.html", message = "Unsuccessful. You entered an incorrect password.")
+
 @app.route("/logout")
 def logout():
     session.pop('user', None)
