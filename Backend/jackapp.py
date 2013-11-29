b17ebd959ac3249029691277f1657a52f0a4a84a
Cahnja <cahnja@gmail.com>
2013-11-29 16:07:39 -0500

    Update jackapp.py

--- Backend/jackapp.py
+++ Backend/jackapp.py
@@ -1,173 +1,38 @@
 #!/usr/bin/python
 
-#goal: to create an ability to export data from the education api 
-#and host it on our website. 
+from flask import Flask, session, redirect, request, url_for, render_template
+from education_backend import run, citysearch
+import json 
 
-import json #download json and then import it in order to access functionality.
-from urllib2 import urlopen
+app = Flask(__name__)
+app.secret_key = "secretkey"
 
-def run():
-    key = "2db9eedc16ba9389991b223c4fb97df8"
-    key1 = "7d733591e1f66640dc372fcb658fe38d"
-    url = "http://api.education.com/service/service.php?f=schoolSearch&key=7d733591e1f66640dc372fcb658fe38d&sn=sf&v=4&city=san+francisco&state=ca&resf=json"
-    print url
-    input1 = urlopen(url)
-    json_raw = input1.read() 
-    json_data = json.loads(json_raw)
+@app.route("/")
+@app.route("/home")
+def home(): 
+    return "<h1>Home</h1>"
 
-    print json_data
-    #for x in range (0, len(json_data)):
-     #   entry = json_data[x]
-      #  entry_info = entry['school']
-       # print "School #" + str(x) + ': ' + str(entry_info) 
+@app.route("/search", methods = ['GET', 'POST'])
+def city_search():
+    if request.method == 'GET':
+        return render_template("city_search.html", message = "")
+    else: 
+        city = request.form['city']
+        zipcode = request.form['zipcode']
+        state = request.form['state']
+        #return "<h1>Home</h1>"
+        d = citysearch(city, state, zipcode)
+        #return redirect(url_for('results'), d=d)
+        return render_template("results.html", d=json.dumps(d), message = "search complete")
+        print("begin")
+       # print(str(d))
 
-# entry_info['city'], entry_info['schooldistrictname'], entry_info['website']
-
-def citysearch(city, state, zipcode):
-    key = "2db9eedc16ba9389991b223c4fb97df8"
-    key1 = "7d733591e1f66640dc372fcb658fe38d"
-    url = "http://api.education.com/service/service.php?f=schoolSearch&key=7d733591e1f66640dc372fcb658fe38d&sn=sf&v=4"
-
-    #&city=san+francisco&state=ca&resf=json"
-
-   # print url
-    if city != "": 
-        url = url + "&city=" + city
-    if state != "":
-        url = url + "&state=" + state
-    if zipcode != "":
-        url = url + "&zip=" + zipcode
-    url = url + "&resf=json"
-    
-    input1 = urlopen(url)
-    json_raw = input1.read() 
-    json_data = json.loads(json_raw)
-    
-    #return json_data
-    #print json_data
-    #for x in range (0, len(json_data)):
-    #entry = json_data[x]
-    #entry_info = entry['school']
-    #print "School #" + str(x) + ': ' + str(entry_info) 
-
-    results = []
-
-    for entry in json_data:
-        data  = []
-        school = entry['school']
-
-        try: 
-            data.append(school['schoolname'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['schooldistrictname'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['schooltype'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['gradelevel'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['gradesserved'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-
-        #special case
-        try:
-            students = school['enrollment']
-            data.append(students['total'])
-        except: 
-            data.append ('None Available')
-
-        try:             
-            students = school['studentteacherratio']
-            data.append(students['total'])
-        except: 
-            data.append ('None Available')
-            
-        #end of special case
-            
-        try:
-            data.append(school['state'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try:             
-            data.append(school['city'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['zip'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['address'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['phonenumber'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['website'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['url'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['latitude'])
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['longitude'])
-        except:
-            data.append ('None Available')
-        try: 
-            data.append(school['districtid'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['districtleaid'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['schoolid'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['nces_id'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['testrating_image_small'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['testrating_year'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['testrating_image_large'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')
-        try: 
-            data.append(school['testrating_text'].encode ('ascii', 'ignore'))
-        except: 
-            data.append ('None Available')     
-        
-        results.append(data)
-            
-    print results
-    return results
-
-if __name__ == '__main__':
-    #run()
-    citysearch("new+york", "ny", "10022")
+@app.route("/results", methods = ['GET', 'POST'])
+def results(): 
+    if request.method == 'GET': 
+        return render_template("results.html", d=JSON.dumps(d), message = "search complete")
 
 
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0",port=5005)
