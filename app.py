25c41b8f31da018cfa08074f9c42551d622819e2
Cahnja <cahnja@gmail.com>
2013-12-01 14:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,13 +39,14 @@ def login():
     else:
         user = request.form['user'].encode ('ascii',"ignore")
         pw = request.form['pass'].encode ('ascii',"ignore")
-        if db.login(user, pw):
+        if user == "" or pw == "":
+            return render_template("login.html", message = "Please enter your username and password.")
+        elif (db.authenticateRegister(user) == False):
+            return render_template("login.html", message = "Invalid username.Please try again.")
+        elif db.authenticate(user, pw):
             session['user'] = user
             return redirect(url_for('home'))
-        if not db.authenticateRegister(user):
-            return render_template("login.html", message = "invalid username and password combination.")
-        else: 
-            return render_template("login.html", message = "invalid username and password combination.")
+        return render_template("login.html", message = "Invalid username and password combination. Usernames and passwords are case sensitive. Please try again.")
 
 @app.route("/schoolsearch", methods = ['GET', 'POST'])
 def search(): 
