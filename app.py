4643fae5bc041b70f570a9a4a67ca2cbea0bad2d
Cahnja <cahnja@gmail.com>
2013-11-30 19:47:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def login():
         else:
             return render_template("login.html", message = "Invalid username and password combination. Usernames and passwords are case sensitive. Please try again.")
 
-@app.route("/city-search", methods = ['GET', 'POST'])
+@app.route("/city_search", methods = ['GET', 'POST'])
 def city_search():
     if request.method == 'GET':
         return render_template("city_search.html", message = "")
